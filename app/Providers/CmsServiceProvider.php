d654d6ef75c707ce99bc09dcecafa6e6378c0106
marco76tv <marco.sottana@gmail.com>
2025-01-03 22:29:17 +0100

    up

--- app/Providers/CmsServiceProvider.php
+++ app/Providers/CmsServiceProvider.php
@@ -118,19 +118,15 @@ class CmsServiceProvider extends XotBaseServiceProvider
     {
         // $prefix=$this->module_name.'::';
         $prefix = '';
+
         /*
-        LivewireService::registerComponents(
-            base_path('Themes/'.$this->xot->pub_theme.'/Http/Livewire'),
-            'Themes\\'.$this->xot->pub_theme,
-            $prefix,
-        );
-        */
         app(RegisterLivewireComponentsAction::class)
             ->execute(
                 base_path('Themes/'.$this->xot->pub_theme.'/Http/Livewire'),
                 'Themes\\'.$this->xot->pub_theme,
                 $prefix,
             );
+        */
     }
 
     /**
